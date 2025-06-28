054e29980d4ac3d8d7411a3691e9ce46efa5a6f9
CandyDream6 <tgmenggithub@163.com>
2025-06-28 23:18:05 +0800

    first commit

---
+++ src/main/java/com/tgmeng/TgmengApiApplication.java
@@ -0,0 +1,13 @@
+package com.tgmeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgmengApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgmengApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/tgmeng/controller/topsearch/china/SocialMedia.java
@@ -0,0 +1,5 @@
+package com.tgmeng.controller.topsearch.china;
+
+public class SocialMedia {
+    // 社交媒体
+}
