845d08066447d7b2190a74a8b08d9a2437fda229
CandyDream6 <tgmenggithub@163.com>
2025-11-20 12:18:31 +0800

    style:cctv返回的平台名完善

--- src/main/java/com/tgmeng/service/topsearch/Impl/TopSearchCommonServiceImpl.java
+++ src/main/java/com/tgmeng/service/topsearch/Impl/TopSearchCommonServiceImpl.java
@@ -2365,7 +2365,7 @@ public class TopSearchCommonServiceImpl implements ITopSearchCommonService {
             throw new ServerException(ServerExceptionEnum.CCTV_SEARCH_EXCEPTION);
         }
         TopSearchCommonVO topSearchCommonVO = new TopSearchCommonVO(topSearchCommonVOS,
-                DataInfoCardEnum.CCTV.getDescription(),
+                searchTypeCCTVEnum.getDescription(),
                 DataInfoCardEnum.CCTV.getValue(),
                 DataInfoCardEnum.CCTV.getDescription());
         return ResultTemplateBean.success(topSearchCommonVO);
