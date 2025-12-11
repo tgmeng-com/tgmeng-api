4f4c2210b266fefa4ba1f6cf581e125a1a31fa50
Neo Chen <yiqiquhuxi@gmail.com>
2025-12-11 11:14:23 +0800

    feat(database): 集成MyBatis-Plus及配置MySQL数据源
    - 在主应用类中添加@MapperScan注解，启用MyBatis Mapper扫描
    - 配置application.yml，添加MySQL数据源连接信息
    - 在pom.xml中引入mybatis-plus和mysql-connector依赖
    - 编写测试用例测试UserMapper，验证数据库连接及操作
    - 测试用例中查询用户列表并打印，确保MyBatis集成正确工作

---
+++ src/main/java/com/tgmeng/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.tgmeng.mapper;
+
+import com.tgmeng.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author bjsttlp317
+* @description 针对表【user】的数据库操作Mapper
+* @createDate 2025-12-11 11:11:08
+* @Entity com.tgmeng.model.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/com/tgmeng/service/UserService.java
@@ -0,0 +1,13 @@
+package com.tgmeng.service;
+
+import com.tgmeng.model.domain.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author bjsttlp317
+* @description 针对表【user】的数据库操作Service
+* @createDate 2025-12-11 11:11:08
+*/
+public interface UserService extends IService<User> {
+
+}
